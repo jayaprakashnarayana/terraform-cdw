11f450c11f1bea8af8551e74db27abf4a8512961
jayaprakashnarayana <prakash.naguboina@gmail.com>
2022-09-12 02:30:19 +0000

    adding resize git

--- main.tf
+++ main.tf
@@ -4,9 +4,19 @@ terraform {
       source = "kreuzwerker/docker"
       version = "2.21.0"
     }
+    
+    aws = {
+        source ="hashicorp/aws"
+        version = "4.30.0"
+    }
+    
   }
 }
 
 provider "docker" {
   # Configuration options
+}
+
+provider "aws" {
+    #configuration 
 }
